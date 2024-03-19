de526c1e97d9bcc5a39b057fd561b64b9f124f0f
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2024-03-19 14:49:10 +0100

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -7,10 +7,10 @@ def get_latest_version(package_name):
     return response.json()["info"]["version"]
 
 
-with open("driver_list.txt", "r") as file:
+with open("extension_list.txt", "r") as file:
     lines = file.readlines()
 
-with open("driver_list.txt", "w") as file:
+with open("extension_list.txt", "w") as file:
     for line in lines:
         package_name, _ = line.split("==")
         latest_version = get_latest_version(package_name)
